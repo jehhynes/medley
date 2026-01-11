f8dff960da1264f10a9bbee312320ab417229dde
Jon Hynes <jonh@datarg.com>
2026-01-11 14:38:47 -0500

    Support for Vue

--- src/Medley.Web/vite.config.js
+++ src/Medley.Web/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 
 export default defineConfig({
+  plugins: [vue()],
+  
   build: {
     // Output to wwwroot/js/dist
     outDir: 'wwwroot/js/dist',
@@ -9,7 +12,7 @@ export default defineConfig({
     
     // Library mode for building the TipTap editor as a standalone module
     lib: {
-      entry: resolve(__dirname, 'src-js/tiptap-editor.js'),
+      entry: resolve(__dirname, 'src-js/TiptapEditor.vue'),
       name: 'TiptapEditor',
       formats: ['iife'],
       fileName: () => 'tiptap-editor.js'
